8d66685fbb3f0ed360aa09c87baadcb4b565f123
姓名 <lmy@icloud.com>
2023-07-26 14:36:45 +0800

    2620. 计数器

---
+++ 简单/2620. 计数器.ts
@@ -0,0 +1,13 @@
+function createCounter(n: number): () => number {
+  let count: number = n
+  return function () {
+    return count++
+  }
+}
+
+/** 
+* const counter = createCounter(10)
+* counter() // 10
+* counter() // 11
+* counter() // 12
+*/
