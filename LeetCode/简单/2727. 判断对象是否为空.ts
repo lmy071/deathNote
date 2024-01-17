06431f6e17d4c437a6185ff040ef89883cac6a87
Lmy <lmy071@icloud.com>
2024-01-17 09:44:49 +0800

    2727

---
+++ LeetCode/简单/2727. 判断对象是否为空.ts
@@ -0,0 +1,9 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+		for (const k in obj) {
+			return false
+		}
+		return true
+};
