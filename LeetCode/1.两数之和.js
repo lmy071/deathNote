6f809be31710f2f0ab8a32621b2758e2ac55ec88
Lm <lmy071@icloud.com>
2024-11-25 22:05:04 +0800

    1.两数之和2.0

--- LeetCode/1.两数之和.js
+++ LeetCode/1.两数之和.js
@@ -16,8 +16,18 @@ var twoSum = function(nums, target) {
 						if((nums[i] + nums[index]) === target){
 							return [i,index]
 						}
-				}		
+				}
 	 }
 };
 // @lc code=end
+
+twoSum = function(nums, target) {
+	let map = new Map
+	for (let i = 0; i < nums.length; i++) {
+		if(map.has(target- nums[i])){
+			return [i,map.get(target- nums[i])]
+		}
+		map.set(nums[i],i)
+	}
+};
 
